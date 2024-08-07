dbbe0cc6960278834efd42f2dc189a338f745030
GOBIERNO1\carlos.caamalc <carloscaamal3@gmail.com>
2024-08-07 12:16:22 -0600

    change: "conexion sql"

--- v1/datos/login_mysql.php
+++ v1/datos/login_mysql.php
@@ -14,10 +14,10 @@ define("USUARIO", "root"); // define nombre del usuario
 define("CONTRASENA", "rgala75"); // define constraseña
 */
 
-define("NOMBRE_HOST", "70.35.201.229:33306"); // define nombre del host
-define("BASE_DE_DATOS", "ivey_sicp"); // define nombre de la base de modelos
-define("USUARIO", "admin"); // define nombre del usuario
-define("CONTRASENA", "WCxaH82XOT3Y"); // define constraseÃ±a*/
+define("NOMBRE_HOST", "74.208.7.151:3306"); // define nombre del host
+define("BASE_DE_DATOS", "sys_atc"); // define nombre de la base de modelos
+define("USUARIO", "carlos"); // define nombre del usuario
+define("CONTRASENA", "admin"); // define constraseÃ±a*/
 
 /*
 define("NOMBRE_HOST", "70.35.201.229:33306"); // define nombre del host
